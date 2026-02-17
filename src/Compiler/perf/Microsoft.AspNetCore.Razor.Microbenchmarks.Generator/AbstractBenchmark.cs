7b20b808cb716efd5705a9e8b758b38a043dfbbc
Chris Sienkiewicz <chsienki@microsoft.com>
2026-02-17 12:21:31 -0800

    more benchmark stuff

--- src/Compiler/perf/Microsoft.AspNetCore.Razor.Microbenchmarks.Generator/AbstractBenchmark.cs
+++ src/Compiler/perf/Microsoft.AspNetCore.Razor.Microbenchmarks.Generator/AbstractBenchmark.cs
@@ -1,6 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using BenchmarkDotNet.Attributes;
 using Microsoft.Build.Locator;
@@ -22,6 +23,7 @@ public abstract class AbstractBenchmark
     [GlobalSetup]
     public void Setup()
     {
+        //Debugger.Launch();
         _project = ProjectSetup.GetRazorProject(Cold);
     }
 
